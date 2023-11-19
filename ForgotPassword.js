6c08ea50851dceefa61f828653728b96f0d86e24
Adnanm05 <150962460+Adnanm05@users.noreply.github.com>
2023-11-19 01:00:39 -0500

    Update ForgotPassword.js

--- ForgotPassword.js
+++ ForgotPassword.js
@@ -1,34 +1,42 @@
 function theQuestion() {
 
+
     var theSchool = document.getElementById("schoolQuestion").value;
     var theMascot = document.getElementById("mascotQuestion").value;
     var theLecture = document.getElementById("lectureQuestion").value;
-
+ 
+ 
     if (theSchool !== "York" || theMascot !== "Lions" || theLecture !== "William Small Center") {
-        
+       
         document.getElementById("demo").innerHTML = "One or More of these Answers are Incorrect";
-
+ 
+ 
     } else {
-
+ 
+ 
         var newPassword = prompt("What Would You Like Your New Password to Be: ");
         var secondTime = prompt("Please Type in Your new Password Again: ");
-
-        if (newPassword !== secondTime) {
-
+ 
+ 
+        while (newPassword !== secondTime) {
+ 
             alert("Your Passwords do not Match");
-            var newPassword = prompt("What Would You Like Your New Password to Be: ");
-            var secondTime = prompt("Please Type in Your new Password Again: ");
-
+            newPassword = prompt("What Would You Like Your New Password to Be: ");
+            secondTime = prompt("Please Type in Your new Password Again: ");
+ 
         }
             alert("Congratulations, Your new Password is Now Set!");
             var savePassword = prompt("Would You Like to Save Your Password: ");
-
+ 
             if (savePassword === "Yes" || savePassword === "yes" || savePassword === "YES") {
                 alert("Perfect, Your Password is now Saved!");
             } else {
                 alert("Great You are Set to Continue! Please Remember Your Password!");
             }
-            
+           
             window.location.href = "https://www.yorku.ca/";
         }
     }
+ 
+ 
+ 

388fcea450eb79e5956fb91780d5f86a0a1ccddd
M1ken1ke <148833685+M1ken1ke@users.noreply.github.com>
2023-11-19 05:23:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ function checkPassword() {
     
     var thePassword = document.getElementById("password").value;
     
-    if (thePassword !== "qz489trfZWwsi6d") {
+    if (thePassword == "qz489trfZWwsi6d") {
+
+         window.location.href = "https://www.yorku.ca/";
+
+    }
+    
+    else {
 
         document.getElementById("demo").innerHTML = "Incorrect! Please Try Again";
-
-    } else {
-
-        window.location.href = "https://www.yorku.ca/";
+       
     }
 }

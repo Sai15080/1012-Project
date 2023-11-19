b7525a4d5728e53da7c8387cf81a226351ea7c6b
Adnanm05 <150962460+Adnanm05@users.noreply.github.com>
2023-11-18 23:23:46 -0500

    Create ForgotPassword.js

---
+++ ForgotPassword.js
@@ -0,0 +1,34 @@
+function theQuestion() {
+
+    var theSchool = document.getElementById("schoolQuestion").value;
+    var theMascot = document.getElementById("mascotQuestion").value;
+    var theLecture = document.getElementById("lectureQuestion").value;
+
+    if (theSchool !== "York" || theMascot !== "Lions" || theLecture !== "William Small Center") {
+        
+        document.getElementById("demo").innerHTML = "One or More of these Answers are Incorrect";
+
+    } else {
+
+        var newPassword = prompt("What Would You Like Your New Password to Be: ");
+        var secondTime = prompt("Please Type in Your new Password Again: ");
+
+        while (newPassword !== secondTime) {
+
+            alert("Your Passwords do not Match");
+            prompt("What Would You Like Your New Password to Be: ");
+            prompt("Please Type in Your new Password Again: ");
+
+        }
+            alert("Congratulations, Your new Password is Now Set!");
+            var savePassword = prompt("Would You Like to Save Your Password: ");
+
+            if (savePassword === "Yes" || savePassword === "yes" || savePassword === "YES") {
+                alert("Perfect, Your Password is now Saved!");
+            } else {
+                alert("Great You are Set to Continue! Please Remember Your Password!");
+            }
+            
+            window.location.href = "https://www.yorku.ca/";
+        }
+    }

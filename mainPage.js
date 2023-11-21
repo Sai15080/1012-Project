d8834395384d9b60b31e28ca6a9460a205f490a1
M1ken1ke <148833685+M1ken1ke@users.noreply.github.com>
2023-11-21 07:11:52 -0500

    Create mainPage.js

---
+++ mainPage.js
@@ -0,0 +1,13 @@
+function toggleTheme(value) { 
+
+     
+    var sheets = document 
+        .getElementsByTagName('link'); 
+
+    sheets[0].href = value; 
+} 
+
+function entryT() {
+
+    window.location.href = "https://www.yorku.ca/";
+}
